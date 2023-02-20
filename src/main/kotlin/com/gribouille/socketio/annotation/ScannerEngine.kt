51956caa668f7bbab2dab461c77f854b4eb11a1d
rlaisqls <rlaisqls@gmail.com>
2023-02-20 10:07:28 +0900

    refactor: annotationScanner 병합

--- src/main/kotlin/com/gribouille/socketio/annotation/ScannerEngine.kt
+++ src/main/kotlin/com/gribouille/socketio/annotation/ScannerEngine.kt
@@ -1,4 +1,3 @@
-
 package com.gribouille.socketio.annotation
 
 import com.gribouille.socketio.namespace.Namespace
@@ -8,73 +7,26 @@ import java.lang.reflect.Modifier
 import java.util.*
 
 class ScannerEngine {
-    private fun findSimilarMethod(objectClazz: Class<*>, method: Method): Method? {
-        val methods = objectClazz.declaredMethods
-        for (m in methods) {
-            if (isEquals(m, method)) {
-                return m
-            }
-        }
-        return null
-    }
 
     @Throws(IllegalArgumentException::class)
     fun scan(namespace: Namespace, obj: Any, clazz: Class<*>) {
         val methods = clazz.declaredMethods
-        if (!clazz.isAssignableFrom(obj.javaClass)) {
-            for (method in methods) {
-                for (annotationScanner in annotations) {
-                    val ann = method.getAnnotation(annotationScanner.scanAnnotation)
-                    if (ann != null) {
-                        annotationScanner.validate(method, clazz)
-                        val m = findSimilarMethod(obj.javaClass, method)
-                        if (m != null) {
-                            annotationScanner.addListener(namespace, obj, m, ann)
-                        } else {
-                            log.warn("Method similar to " + method.name + " can't be found in " + obj.javaClass)
-                        }
-                    }
+        for (method in methods) {
+            for (annotation in SocketAnnotation.annotations) {
+                method.getAnnotation(annotation.java)?.let {
+                    SocketAnnotationScanner.addListener(
+                        namespace = namespace,
+                        obj = obj,
+                        method = method,
+                        annotation = it
+                    )
+                    log.info("add {} Listener class: {} method: {}", annotation, obj.javaClass, method.name)
                 }
             }
-        } else {
-            for (method in methods) {
-                for (annotationScanner in annotations) {
-                    val ann = method.getAnnotation(annotationScanner.scanAnnotation)
-                    if (ann != null) {
-                        annotationScanner.validate(method, clazz)
-                        makeAccessible(method)
-                        annotationScanner.addListener(namespace, obj, method, ann)
-                    }
-                }
-            }
-            if (clazz.superclass != null) {
-                scan(namespace, obj, clazz.superclass)
-            } else if (clazz.isInterface) {
-                for (superIfc in clazz.interfaces) {
-                    scan(namespace, obj, superIfc)
-                }
-            }
-        }
-    }
-
-    private fun isEquals(method1: Method, method2: Method): Boolean {
-        return if (method1.name != method2.name
-            || method1.returnType != method2.returnType
-        ) {
-            false
-        } else Arrays.equals(method1.parameterTypes, method2.parameterTypes)
-    }
-
-    private fun makeAccessible(method: Method) {
-        if ((!Modifier.isPublic(method.modifiers) || !Modifier.isPublic(method.declaringClass.modifiers))
-            && !method.isAccessible
-        ) {
-            method.isAccessible = true
         }
     }
 
     companion object {
         private val log = LoggerFactory.getLogger(ScannerEngine::class.java)
-        private val annotations = Arrays.asList(OnConnectScanner(), OnDisconnectScanner(), OnEventScanner())
     }
 }
---
+++ src/main/kotlin/com/gribouille/socketio/annotation/SocketAnnotationScanner.kt
@@ -0,0 +1,76 @@
+package com.gribouille.socketio.annotation
+
+import com.gribouille.socketio.AckRequest
+import com.gribouille.socketio.SocketIOClient
+import com.gribouille.socketio.handler.SocketIOException
+import com.gribouille.socketio.listener.ConnectListener
+import com.gribouille.socketio.listener.DataListener
+import com.gribouille.socketio.namespace.Namespace
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+
+object SocketAnnotationScanner {
+
+    fun addListener(
+        namespace: Namespace,
+        obj: Any,
+        method: Method,
+        annotation: Annotation
+    ) {
+        when (annotation.annotationClass) {
+            OnConnect::class -> addConnectionListener(namespace, obj, method, annotation)
+            OnDisconnect::class -> addConnectionListener(namespace, obj, method, annotation)
+            OnEvent::class -> addOnEventListener(namespace, obj, method, annotation)
+        }
+    }
+
+    private fun addConnectionListener(namespace: Namespace, obj: Any, method: Method, annotation: Annotation) {
+        namespace.addConnectListener(object : ConnectListener {
+            override fun onConnect(client: SocketIOClient?) {
+                try {
+                    method.invoke(obj, client)
+                } catch (e: InvocationTargetException) {
+                    throw SocketIOException(e.cause)
+                } catch (e: Exception) {
+                    throw SocketIOException(e)
+                }
+            }
+        })
+    }
+
+    private fun addOnEventListener(namespace: Namespace, obj: Any?, method: Method, annot: Annotation) {
+
+        val annotation = annot as OnEvent
+        require(annotation.value.trim { it <= ' ' }.isNotEmpty()) { "OnEvent \"value\" parameter is required" }
+
+        val requestType = getDataType(method)
+        namespace.addEventListener(annotation.value, requestType, object : DataListener {
+            override fun onData(client: SocketIOClient, data: Any, ackSender: AckRequest) {
+                try {
+                    val args: MutableList<Any> = ArrayList()
+                    for (params in method.parameterTypes) {
+                        if (params == SocketIOClient::class.java) {
+                            args.add(client)
+                        } else if (params == requestType) {
+                            args.add(data)
+                        }
+                    }
+                    method.invoke(obj, *args.toTypedArray())
+                } catch (e: InvocationTargetException) {
+                    throw SocketIOException(e.cause)
+                } catch (e: Exception) {
+                    throw SocketIOException(e)
+                }
+            }
+        })
+    }
+
+    private fun getDataType(method: Method): Class<*> {
+        for (type in method.parameterTypes) {
+            if (type != AckRequest::class.java && type != SocketIOClient::class.java) {
+                return type
+            }
+        }
+        return Void::class.java
+    }
+}
