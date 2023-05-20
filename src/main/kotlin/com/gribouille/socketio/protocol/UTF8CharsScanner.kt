bfcc27251221439a707653de9c9fdd33cb0e3705
rlaisqls <rlaisqls@gmail.com>
2023-05-20 14:49:49 +0900

    refactor: object로 변경

--- src/main/kotlin/com/gribouille/socketio/protocol/UTF8CharsScanner.kt
+++ src/main/kotlin/com/gribouille/socketio/protocol/UTF8CharsScanner.kt
@@ -3,7 +3,7 @@ package com.gribouille.socketio.protocol
 
 import io.netty.buffer.ByteBuf
 
-class UTF8CharsScanner {
+object UTF8CharsScanner {
 
     fun getActualLength(inputBuffer: ByteBuf, length: Int): Int {
         var len = 0
@@ -32,41 +32,39 @@ class UTF8CharsScanner {
         return idx
     }
 
-    companion object {
-        /**
-         * 텍스트 세그먼트에 특수 처리가 필요한 입력 문자가 포함될때 사용하는 lockup table
-         */
-        private val sInputCodes: IntArray = let {
-            val table = IntArray(256)
-            // Control chars와 space가 아닌 공백은 따옴표 없이 사용할 수 없다.
-            for (i in 0..31) {
-                table[i] = -1
-            }
-            // string end와 따옴표 처리
-            table['"'.code] = 1
-            table['\\'.code] = 1
-            table
+    /**
+     * 텍스트 세그먼트에 특수 처리가 필요한 입력 문자가 포함될때 사용하는 lockup table
+     */
+    private val sInputCodes: IntArray = let {
+        val table = IntArray(256)
+        // Control chars와 space가 아닌 공백은 따옴표 없이 사용할 수 없다.
+        for (i in 0..31) {
+            table[i] = -1
         }
+        // string end와 따옴표 처리
+        table['"'.code] = 1
+        table['\\'.code] = 1
+        table
+    }
 
-        /**
-         * UTF-8 decoding에 추가적으로 필요한 lockup table
-         */
-        val sInputCodesUtf8: IntArray
+    /**
+     * UTF-8 decoding에 추가적으로 필요한 lockup table
+     */
+    private val sInputCodesUtf8: IntArray
 
-        init {
-            val table = IntArray(sInputCodes.size)
-            System.arraycopy(sInputCodes, 0, table, 0, sInputCodes.size)
-            for (c in 128..255) {
+    init {
+        val table = IntArray(sInputCodes.size)
+        System.arraycopy(sInputCodes, 0, table, 0, sInputCodes.size)
+        for (c in 128..255) {
 
-                // decoding에 필요한 바이트 수를 나타낸다.
-                val code = if (c and 0xE0 == 0xC0) 2 // 2 bytes (0x0080 - 0x07FF)
-                else if (c and 0xF0 == 0xE0) 3 // 3 bytes (0x0800 - 0xFFFF)
-                else if (c and 0xF8 == 0xF0) 4 // 4 bytes; double-char with surrogates and all...
-                else -1 // error marker
+            // decoding에 필요한 바이트 수를 나타낸다.
+            val code = if (c and 0xE0 == 0xC0) 2 // 2 bytes (0x0080 - 0x07FF)
+            else if (c and 0xF0 == 0xE0) 3 // 3 bytes (0x0800 - 0xFFFF)
+            else if (c and 0xF8 == 0xF0) 4 // 4 bytes; double-char with surrogates and all...
+            else -1 // error marker
 
-                table[c] = code
-            }
-            sInputCodesUtf8 = table
+            table[c] = code
         }
+        sInputCodesUtf8 = table
     }
 }
